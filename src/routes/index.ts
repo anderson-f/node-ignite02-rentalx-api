ca2c844808a47b74b7822f1d7fc162143614c7a8
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-07-11 17:14:31 -0300

    feat: Creating user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 // toda vez que bater na url /categories o use vai direcionar pro arquivo de categorias
 router.use("/categories", categoriesRoutes); // middleware da rota categories
 router.use("/specifications", specificationsRoutes); // middleware da rota categories
+router.use("/users", usersRoutes);
 
 export { router };
